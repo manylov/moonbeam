86845b5108cbd6ec0a3d4cdb62c3be2c85acc06f
4meta5 <asinghchrony@protonmail.com>
2022-04-01 14:29:28 -0400

    in progress will rebase against master soon

--- pallets/randomness/src/lib.rs
+++ pallets/randomness/src/lib.rs
@@ -31,12 +31,110 @@ pub use pallet::*;
 // #[cfg(test)]
 // mod tests;
 
+// need to also store the CURRENT_EPOCH
+// could just get the next one if
+
 #[pallet]
 pub mod pallet {
 	// use crate::WeightInfo;
 	use frame_support::pallet_prelude::*;
 	use frame_support::traits::{Currency, ReservableCurrency};
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::{CheckedSub, Saturating};
+
+	#[derive(PartialEq, Copy, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+	/// Randomness storage item from BABE
+	pub enum BabeRandomness {
+		OneEpochAgo,
+		TwoEpochsAgo,
+		CurrentBlock,
+	}
+
+	#[derive(PartialEq, Copy, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+	/// Information regarding the request type
+	pub enum RequestType<BlockNumber> {
+		/// BABE randomness from reading relay chain state proof
+		Babe {
+			info: BabeRandomness,
+			when: BlockNumber,
+		},
+		/// Per block VRF randomness
+		Local { when: BlockNumber },
+	}
+
+	impl<BlockNumber: Copy> RequestType<BlockNumber> {
+		pub fn when(&self) -> BlockNumber {
+			match self {
+				RequestType::Babe { when, .. } => *when,
+				RequestType::Local { when } => *when,
+			}
+		}
+	}
+
+	#[derive(PartialEq, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Request<T: Config> {
+		/// Fee is returned to this account upon execution
+		pub refund_address: T::AccountId,
+		/// Contract that consumes the randomness
+		pub contract_address: T::AccountId,
+		/// Fee to pay for execution
+		pub fee: BalanceOf<T>,
+		/// Salt to use once randomness is ready
+		pub salt: T::Hash,
+		/// Details regarding request type and when it is due
+		pub info: RequestType<T::BlockNumber>,
+		/// All requests expire `T::ExpirationDelay` blocks after they are made
+		pub expires: T::BlockNumber,
+	}
+
+	impl<T: Config> Request<T> {
+		fn can_be_fulfilled(&self) -> bool {
+			self.info.when() <= frame_system::Pallet::<T>::block_number()
+		}
+		fn fulfill(&self) -> DispatchResult {
+			ensure!(
+				self.can_be_fulfilled(),
+				Error::<T>::RequestCannotYetBeFulfilled
+			);
+			let raw_randomness: T::Hash = match self.info {
+				RequestType::Local { .. } => return Err(Error::<T>::NotYetImplemented.into()),
+				RequestType::Babe { info, .. } => {
+					Pallet::<T>::get_most_recent_babe_randomness(info)
+				}
+			};
+			let randomness = Pallet::<T>::concat_and_hash(raw_randomness, self.salt);
+			T::RandomnessSender::send_randomness(self.contract_address.clone(), randomness);
+			Ok(())
+		}
+		fn increase_fee(&mut self, caller: &T::AccountId, new_fee: BalanceOf<T>) -> DispatchResult {
+			ensure!(
+				caller == &self.contract_address,
+				Error::<T>::OnlyRequesterCanIncreaseFee
+			);
+			let to_reserve = new_fee
+				.checked_sub(&self.fee)
+				.ok_or(Error::<T>::NewFeeMustBeGreaterThanOldFee)?;
+			T::Currency::reserve(caller, to_reserve)?;
+			self.fee = new_fee;
+			Ok(())
+		}
+	}
+
+	/// Send randomness to a smart contract, triggered by this pallet
+	pub trait SendRandomness<AccountId, R> {
+		fn send_randomness(contract: AccountId, randomness: R);
+	}
+
+	/// Get babe randomness to insert into runtime
+	pub trait GetRelayRandomness<R> {
+		fn get_current_block_randomness() -> (R, Weight);
+		fn get_one_epoch_ago_randomness() -> (R, Weight);
+		fn get_two_epochs_ago_randomness() -> (R, Weight);
+	}
+
+	/// Request identifier, unique per request for randomness
+	pub type RequestId = u64;
 
 	pub type BalanceOf<T> =
 		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
@@ -45,9 +143,6 @@ pub mod pallet {
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
-	/// Request identifier, unique per request for randomness
-	pub type RequestId = u64;
-
 	/// Configuration trait of this pallet.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -55,16 +150,32 @@ pub mod pallet {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 		/// Currency in which the security deposit will be taken.
 		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+		/// Get relay chain randomness to insert into this pallet
+		type RelayRandomness: GetRelayRandomness<Self::Hash>;
+		/// Send randomness to smart contract
+		/// TODO: why can't Randomness = T::Hash??
+		type RandomnessSender: SendRandomness<Self::AccountId, [u8; 32]>;
+		#[pallet::constant]
 		/// The amount that should be taken as a security deposit when requesting randomness.
 		type Deposit: Get<BalanceOf<Self>>;
+		#[pallet::constant]
+		/// Number of blocks after a request is made when it can be purged from storage
+		type ExpirationDelay: Get<Self::BlockNumber>;
 		// /// Weight information for extrinsics in this pallet.
 		// type WeightInfo: WeightInfo;
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
+		NotYetImplemented,
 		/// The counter for request IDs overflowed
 		RequestCounterOverflowed,
+		NotSufficientDeposit,
+		CannotRequestPastRandomness,
+		RequestDNE,
+		RequestCannotYetBeFulfilled,
+		OnlyRequesterCanIncreaseFee,
+		NewFeeMustBeGreaterThanOldFee,
 	}
 
 	#[pallet::event]
@@ -72,82 +183,208 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// Randomness requested and request put in storage
 		RandomnessRequested {
+			id: RequestId,
 			refund_address: T::AccountId,
 			contract_address: T::AccountId,
-			fulfillment_fee: BalanceOf<T>,
-			request_id: RequestId,
+			fee: BalanceOf<T>,
+			salt: T::Hash,
+			info: RequestType<T::BlockNumber>,
+		},
+		RequestFulfilled {
+			id: RequestId,
+		},
+		RequestFeeIncreased {
+			id: RequestId,
+			new_fee: BalanceOf<T>,
 		},
 	}
 
 	#[pallet::storage]
 	#[pallet::getter(fn request)]
 	/// Randomness requests not yet fulfilled or purged
-	pub type Request<T: Config> =
-		StorageMap<_, Blake2_128Concat, RequestId, RequestId, OptionQuery>;
+	pub type Requests<T: Config> =
+		StorageMap<_, Blake2_128Concat, RequestId, Request<T>, OptionQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn request_count)]
 	/// Number of randomness requests made so far, used to generate the next request's uid
 	pub type RequestCount<T: Config> = StorageValue<_, RequestId, ValueQuery>;
 
-	// #[pallet::genesis_config]
-	// /// Genesis config for author mapping pallet
-	// pub struct GenesisConfig<T: Config> {
-	// 	/// The associations that should exist at chain genesis
-	// 	pub mappings: Vec<(NimbusId, T::AccountId)>,
-	// }
+	#[pallet::storage]
+	#[pallet::getter(fn current_block_randomness)]
+	/// Relay chain current block randomness
+	/// TODO: replace with ParentBlockRandomness once
+	/// https://github.com/paritytech/substrate/pull/11113 is merged
+	pub type CurrentBlockRandomness<T: Config> = StorageValue<_, T::Hash, ValueQuery>;
 
-	// #[cfg(feature = "std")]
-	// impl<T: Config> Default for GenesisConfig<T> {
-	// 	fn default() -> Self {
-	// 		Self { mappings: vec![] }
-	// 	}
-	// }
+	#[pallet::storage]
+	#[pallet::getter(fn last_current_block_randomness)]
+	/// Last relay chain current block randomness
+	/// TODO: replace with LastParentBlockRandomness once
+	/// https://github.com/paritytech/substrate/pull/11113 is merged
+	pub type LastCurrentBlockRandomness<T: Config> = StorageValue<_, T::Hash, ValueQuery>;
 
-	// #[pallet::genesis_build]
-	// impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
-	// 	fn build(&self) {
-	// 		for (author_id, account_id) in &self.mappings {
-	// 			if let Err(e) = Pallet::<T>::enact_registration(&author_id, &account_id) {
-	// 				log::warn!("Error with genesis author mapping registration: {:?}", e);
-	// 			}
-	// 		}
-	// 	}
-	// }
+	#[pallet::storage]
+	#[pallet::getter(fn one_epoch_ago_randomness)]
+	/// Relay chain one epoch ago randomness
+	pub type OneEpochAgoRandomness<T: Config> = StorageValue<_, T::Hash, ValueQuery>;
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// Make randomness request
-		#[pallet::weight(0)]
-		pub fn request_randomness(
-			origin: OriginFor<T>,
-			source_id: u32, // what type here
-			refund_address: T::AccountId,
-			contract_address: T::AccountId,
-			salt: [u8; 32],
-			fulfillment_fee: BalanceOf<T>,
-		) -> DispatchResult {
-			ensure_signed(origin)?;
-			// TODO: just emit error if this overflows instead of saturating?
-			let new_id = <RequestCount<T>>::get()
-				.checked_add(1u64)
-				.ok_or(Error::<T>::RequestCounterOverflowed)?;
-			<RequestCount<T>>::put(new_id);
-			Self::deposit_event(Event::RandomnessRequested {
-				refund_address,
-				contract_address,
-				fulfillment_fee,
-				request_id: new_id,
-			});
-			Ok(())
+	#[pallet::storage]
+	#[pallet::getter(fn last_one_epoch_ago_randomness)]
+	/// Last relay chain one epoch ago randomness
+	pub type LastOneEpochAgoRandomness<T: Config> = StorageValue<_, T::Hash, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn two_epochs_ago_randomness)]
+	/// Relay chain two epochs ago randomness
+	pub type TwoEpochsAgoRandomness<T: Config> = StorageValue<_, T::Hash, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn last_two_epochs_ago_randomness)]
+	/// Last relay chain two epochs ago randomness
+	pub type LastTwoEpochsAgoRandomness<T: Config> = StorageValue<_, T::Hash, ValueQuery>;
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(_now: BlockNumberFor<T>) -> Weight {
+			// Get randomness from runtime and set it locally
+			// TODO: optimize by writing a PushRandomness trait which pushes to this pallet when
+			// the item is updated instead...Need to know when the epoch changes from relay proof.
+			let (
+				last_current_block_randomness,
+				last_one_epoch_ago_randomness,
+				last_two_epochs_ago_randomness,
+			) = (
+				<CurrentBlockRandomness<T>>::get(),
+				<OneEpochAgoRandomness<T>>::get(),
+				<TwoEpochsAgoRandomness<T>>::get(),
+			);
+			<LastCurrentBlockRandomness<T>>::put(last_current_block_randomness);
+			<LastOneEpochAgoRandomness<T>>::put(last_one_epoch_ago_randomness);
+			<LastTwoEpochsAgoRandomness<T>>::put(last_two_epochs_ago_randomness);
+			let (current_block_randomness, current_block_randomness_wt) =
+				T::RelayRandomness::get_current_block_randomness();
+			let (one_epoch_ago_randomness, one_epoch_ago_randomness_wt) =
+				T::RelayRandomness::get_one_epoch_ago_randomness();
+			let (two_epochs_ago_randomness, two_epochs_ago_randomness_wt) =
+				T::RelayRandomness::get_two_epochs_ago_randomness();
+			<CurrentBlockRandomness<T>>::put(current_block_randomness);
+			<OneEpochAgoRandomness<T>>::put(one_epoch_ago_randomness);
+			<TwoEpochsAgoRandomness<T>>::put(two_epochs_ago_randomness);
+			3 * T::DbWeight::get().read
+				+ 3 * T::DbWeight::get().write
+				+ current_block_randomness_wt
+				+ one_epoch_ago_randomness_wt
+				+ two_epochs_ago_randomness_wt
 		}
 	}
 
+	// Utility functions
 	impl<T: Config> Pallet<T> {
-		/// A helper function to lookup the account id associated with the given author id. This is
-		/// the primary lookup that this pallet is responsible for.
-		pub fn todo() {
-			todo!()
+		fn get_most_recent_babe_randomness(b: BabeRandomness) -> T::Hash {
+			match b {
+				BabeRandomness::OneEpochAgo => <OneEpochAgoRandomness<T>>::get(),
+				BabeRandomness::TwoEpochsAgo => <TwoEpochsAgoRandomness<T>>::get(),
+				BabeRandomness::CurrentBlock => <CurrentBlockRandomness<T>>::get(),
+			}
+		}
+		fn concat_and_hash(randomness: T::Hash, salt: T::Hash) -> [u8; 32] {
+			let mut s = Vec::new();
+			s.extend_from_slice(randomness.as_ref());
+			s.extend_from_slice(salt.as_ref());
+			sp_io::hashing::blake2_256(&s)
+		}
+	}
+
+	// This is where we expose pallet functionality for the precompile
+	impl<T: Config> Pallet<T> {
+		// TODO: separate Expires field from request
+		pub fn request_randomness(request: Request<T>) -> DispatchResult {
+			ensure!(
+				!request.can_be_fulfilled(),
+				Error::<T>::CannotRequestPastRandomness
+			);
+			let deposit = T::Deposit::get().saturating_add(request.fee);
+			// is the calling contract always the consuming contract??
+			// or can the depositer be different from consumer
+			T::Currency::can_reserve(&request.contract_address, deposit)
+				.then(|| true)
+				.ok_or(Error::<T>::NotSufficientDeposit)?;
+			// get new request ID
+			let request_id = <RequestCount<T>>::get();
+			let next_id = request_id
+				.checked_add(1u64)
+				.ok_or(Error::<T>::RequestCounterOverflowed)?;
+			T::Currency::reserve(&request.contract_address, deposit)?;
+			// insert request
+			<RequestCount<T>>::put(next_id);
+			Self::deposit_event(Event::RandomnessRequested {
+				id: request_id,
+				refund_address: request.refund_address.clone(),
+				contract_address: request.contract_address.clone(),
+				fee: request.fee,
+				salt: request.salt,
+				info: request.info,
+			});
+			<Requests<T>>::insert(request_id, request);
+			Ok(())
+		}
+		/// Execute fulfillment for randomness if it is due
+		// TODO: fee management
+		// execution costs - `request.fee` is refunded to `refund_address`
+		// cost of execution refunded to the caller?
+		pub fn execute_fulfillment(caller: T::AccountId, id: RequestId) -> DispatchResult {
+			let request = <Requests<T>>::get(id).ok_or(Error::<T>::RequestDNE)?;
+			// fulfill randomness request
+			request.fulfill()?;
+			// return deposit to contract_address
+			T::Currency::unreserve(&request.contract_address, T::Deposit::get());
+			<Requests<T>>::remove(id);
+			Self::deposit_event(Event::RequestFulfilled { id });
+			Ok(())
+		}
+		pub fn increase_request_fee(
+			caller: T::AccountId,
+			id: RequestId,
+			new_fee: BalanceOf<T>,
+		) -> DispatchResult {
+			let mut request = <Requests<T>>::get(id).ok_or(Error::<T>::RequestDNE)?;
+			// fulfill randomness request
+			request.increase_fee(&caller, new_fee)?;
+			<Requests<T>>::insert(id, request);
+			Self::deposit_event(Event::RequestFeeIncreased { id, new_fee });
+			Ok(())
+		}
+		/// Execute request expiration
+		/// Pays fee to caller
+		/// Purges request if it has expired
+		pub fn execute_request_expiration(
+			caller: T::AccountId,
+			id: RequestId,
+			new_amount: BalanceOf<T>,
+		) -> DispatchResult {
+			Ok(())
+		}
+		pub fn instant_babe_randomness(
+			contract_address: T::AccountId,
+			babe_randomness: BabeRandomness,
+			salt: T::Hash,
+		) -> DispatchResult {
+			let raw_randomness = Self::get_most_recent_babe_randomness(babe_randomness);
+			let randomness = Self::concat_and_hash(raw_randomness, salt);
+			T::RandomnessSender::send_randomness(contract_address, randomness);
+			Ok(())
+		}
+		pub fn instant_local_randomness(
+			contract_address: T::AccountId,
+			salt: T::Hash,
+		) -> DispatchResult {
+			Err(Error::<T>::NotYetImplemented.into())
 		}
 	}
 }
+
+// need to DO
+// mocks
+// tests
+// precompile
